465ad017e443b69a229abc41274e1c07eb504a8e
Salim Chemes <salim@mobomo.com>
2020-02-08 17:09:32 -0300

    adds fonts and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ProjectComponent } from './pages/project/project.component';
-import { AboutComponent } from './pages/about/about.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { HeroComponent } from "./components/hero/hero.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ProjectComponent } from "./pages/project/project.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -20,11 +21,8 @@ import { AboutComponent } from './pages/about/about.component';
     ProjectComponent,
     AboutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
